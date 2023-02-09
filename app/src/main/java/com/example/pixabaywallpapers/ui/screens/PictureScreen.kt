eb16c03519cc7f96b60bcffcf5bde57e9984701c
yakov <yakovrussian@gmail.com>
2023-02-09 15:15:10 +0300

    Image now scrollable

--- app/src/main/java/com/example/pixabaywallpapers/ui/screens/PictureScreen.kt
+++ app/src/main/java/com/example/pixabaywallpapers/ui/screens/PictureScreen.kt
@@ -1,14 +1,17 @@
 package com.example.pixabaywallpapers.ui.screens
 
 import android.app.WallpaperManager
+import android.content.res.Configuration
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -17,9 +20,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import coil.ImageLoader
 import coil.request.ImageRequest
 import coil.request.SuccessResult
+import com.example.pixabaywallpapers.ui.theme.PixabayWallpapersTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
@@ -43,25 +50,46 @@ fun PictureScreen(
         }
     }
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        Button(
-            onClick = { bm.value?.let { WallpaperManager.getInstance(context).setBitmap(it) } },
-            enabled = bm.value != null,
-            content = { Text("Set as background") }
-        )
-        bm.value?.let {
-            Image(
-                modifier = Modifier.fillMaxSize(),
-                bitmap = it.asImageBitmap(),
-                contentScale = ContentScale.Crop,
-                contentDescription = "Picture Full Size"
-            )
-        } ?: Column(
-            modifier = Modifier.fillMaxSize(),
+    ConstraintLayout {
+        val (pic, button) = createRefs()
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .constrainAs(pic) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                },
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-            CircularProgressIndicator()
+            bm.value?.let {
+                Image(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .horizontalScroll(rememberScrollState()),
+                    bitmap = it.asImageBitmap(),
+                    contentScale = ContentScale.Crop,
+                    contentDescription = "Picture Full Size"
+                )
+            } ?: CircularProgressIndicator()
+        }
+
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.surface.copy(alpha = 0.2f))
+                .constrainAs(button) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                }
+        ) {
+            Button(
+                onClick = { bm.value?.let { WallpaperManager.getInstance(context).setBitmap(it) } },
+                enabled = bm.value != null,
+                content = { Text("Set as background") },
+                modifier = Modifier.padding(8.dp)
+            )
         }
     }
 }
